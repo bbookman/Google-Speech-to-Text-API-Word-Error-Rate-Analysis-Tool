40acc3300adbcf110d47b2453b4c9238fae582dd
Bruce Bookman <bruce.bookman@gmail.com>
2020-01-30 10:14:47 -0800

    tests built and passing for simple speech context data

--- model/speech_context.py
+++ model/speech_context.py
@@ -1,2 +1,9 @@
 class SpeechContext():
-    pass
+    phrases = []
+
+    def set_phrases(self, data):
+        self.phrases = data
+        return self.phrases
+
+    def get_phrases(self):
+        return self.phrases
--- tests/test_phrases.py
+++ tests/test_phrases.py
@@ -3,3 +3,23 @@ import pytest
 def test_speech_context_init():
     from model.speech_context import SpeechContext
 
+def test_set_phrases():
+    from model.speech_context import SpeechContext
+    context = SpeechContext()
+    context.set_phrases("data")
+
+
+def test_get_phrases():
+    from model.speech_context import SpeechContext
+    context = SpeechContext()
+    context.get_phrases()
+
+def test_set_then_get_phrases():
+    from model.speech_context import SpeechContext
+    expected = "data"
+    context = SpeechContext()
+    context.set_phrases(expected)
+    result = context.get_phrases()
+    assert expected == result
+
+    
