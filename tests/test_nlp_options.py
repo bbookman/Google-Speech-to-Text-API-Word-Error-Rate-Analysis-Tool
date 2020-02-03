a23a93a035982a528dd939998b6abb88b5532c77
Bruce Bookman <bruce.bookman@gmail.com>
2020-02-03 15:50:16 -0800

    set get plus expand contrctions all work

---
+++ tests/test_nlp_options.py
@@ -0,0 +1,109 @@
+import pytest
+
+def test_set_should_expand_contractions():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    options.set_should_expand_contractions(True)
+
+def test_get_should_expand_contractions():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    result = options.get_should_expand_contractions()
+    assert isinstance(result, bool)
+
+def test_set_get_should_expand_contractions():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    expected = True
+    options.set_should_expand_contractions(expected)
+    result = options.get_should_expand_contractions()
+    assert result == expected
+
+def test_set_should_apply_stemming():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    options.set_should_apply_stemming(False)
+
+def test_get_should_apply_stemming():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    result = options.get_should_apply_stemming()
+    assert isinstance(result, bool)
+
+def test_set_get_should_apply_stemming():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    expected = False
+    options.set_should_apply_stemming(expected)
+    result = options.get_should_apply_stemming()
+    assert result == expected
+
+def test_set_should_convert_numbers_to_words():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    options.set_should_convert_numbers_to_words(True)
+
+def test_get_should_convert_numbers_to_words():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    result = options.get_should_convert_numbers_to_words()
+    assert isinstance(result, bool)
+
+def test_set_get_should_convert_numbers_to_words():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    expected = True
+    options.set_should_convert_numbers_to_words(expected)
+    result = options.get_should_convert_numbers_to_words()
+    assert result == expected
+
+def test_set_should_remove_stop_words():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    options.set_should_remove_stop_words(False)
+
+def test_get_should_remove_stop_words():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    result = options.get_should_remove_stop_words()
+    assert isinstance(result, bool)
+
+def test_set_get_should_remove_stop_words():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    expected = True
+    options.set_should_remove_stop_words(expected)
+    result = options.get_should_remove_stop_words()
+    assert result == expected
+
+def test_nlp_options_init():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    result = options.contractions
+    assert isinstance(result, dict)
+    for value in result.values():
+        assert isinstance(value, str)
+
+def test_expand_contractions_yall():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    expected = "you all"
+    data = "y'all"
+    result = options.expand_contractions(data)
+    assert result == expected
+
+def test_expand_contractions_yall():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    expected = "you all"
+    data = "y'all"
+    result = options.expand_contractions(data)
+    assert result == expected
+
+def test_expand_contractions_isnt():
+    from utilities.nlp_options import NLPOptions
+    options = NLPOptions()
+    expected = "is not"
+    data = "isn't"
+    result = options.expand_contractions(data)
+    assert result == expected
---
+++ utilities/nlp_options.py
@@ -0,0 +1,57 @@
+class NLPOptions(object):
+    should_expand_contractions = bool()
+    should_apply_stemming = bool()
+    should_convert_numbers_to_words = bool()
+    should_remove_stop_words = bool()
+    contractions = None
+
+    def __init__(self):
+        from utilities.contractions import contractions_dictionary
+        self.contractions = contractions_dictionary
+
+    def set_should_expand_contractions(self, option):
+        self.should_expand_contractions = option
+
+    def get_should_expand_contractions(self):
+        return self.should_expand_contractions
+
+    def set_should_apply_stemming(self, option):
+        self.should_apply_stemming = option
+
+    def get_should_apply_stemming(self):
+        return self.should_apply_stemming
+
+    def set_should_convert_numbers_to_words(self, option):
+        self.should_convert_numbers_to_words = option
+
+    def get_should_convert_numbers_to_words(self):
+        return self.should_convert_numbers_to_words
+
+    def set_should_remove_stop_words(self, option):
+        self.should_remove_stop_words = option
+
+    def get_should_remove_stop_words(self):
+        return self.should_remove_stop_words
+
+
+    def expand_contractions(self, text):
+        import re
+        contractions = self.contractions
+        contractions_pattern = re.compile('({})'.format('|'.join(contractions.keys())),
+                                          flags=re.IGNORECASE | re.DOTALL)
+
+        def expand_match(contraction):
+            match = contraction.group(0)
+            first_char = match[0]
+            expanded_contraction = contractions.get(match) \
+                if contractions.get(match) \
+                else contractions.get(match.lower())
+            expanded_contraction = first_char + expanded_contraction[1:]
+            # Hack for this bug: https://github.com/dipanjanS/practical-machine-learning-with-python/issues/24
+            if expanded_contraction == "as not":
+                return "is not"
+            return expanded_contraction
+
+        expanded_text = contractions_pattern.sub(expand_match, text)
+        expanded_text = re.sub("'", "", expanded_text)
+        return expanded_text
