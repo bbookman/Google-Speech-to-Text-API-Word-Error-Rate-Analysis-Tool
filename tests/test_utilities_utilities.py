9831735c671f358b0a0439111fde06123937562e
Bruce Bookman <bruce.bookman@gmail.com>
2020-02-03 14:09:46 -0800

    all utils working

--- tests/test_utilities_utilities.py
+++ tests/test_utilities_utilities.py
@@ -72,12 +72,19 @@ def test_string_to_enum_speex_with_header_byte():
     result = u.string_to_enum(txt)
     assert result == expected
 
+def test_get_extension():
+    from utilities.utilities import Utilities
+    u = Utilities()
+    expected = 'flac'
+    result = u._get_extension('woooooo.flac')
+    assert result == expected
+
 def test_is_valid_file_extension_false():
     from utilities.utilities import Utilities
     u = Utilities()
     expected = False
     extension = 'doc'
-    result = u.is_valid_file_extension(extension)
+    result = u._is_valid_file_extension(extension)
     assert result == expected
 
 
@@ -86,7 +93,7 @@ def test_is_valid_file_extension_true1():
     u = Utilities()
     expected = True
     extension = 'flac'
-    result = u.is_valid_file_extension(extension)
+    result = u._is_valid_file_extension(extension)
     assert result == expected
 
 
@@ -95,24 +102,24 @@ def test_is_valid_file_extension_true2():
     u = Utilities()
     expected = True
     extension = 'MP3'
-    result = u.is_valid_file_extension(extension)
+    result = u._is_valid_file_extension(extension)
     assert result == expected
 
 def test_is_audio_homogeneous_false():
     from utilities.utilities import Utilities
     u = Utilities()
-    test_list = ['hello.wav', 'hello.mp3', 'wow.flac', 'wow.wav', 'test.txt']
+    test_list = ['gs://hello.wav', 'gs://foo/bar/baz/hello.mp3', 'wow.flac', 'wow.wav', 'test.txt']
     expected = False
-    result = u.is_audio_homogeneous(test_list)
+    result = u._is_audio_homogeneous(test_list)
     assert result == expected
 
 
 def test_is_audio_homogeneous_true1():
     from utilities.utilities import Utilities
     u = Utilities()
-    test_list = ['hello.wav', 'wow.wav', 'test.txt']
+    test_list = ['gs://foo/hello.wav', 'gs://foo/bar/wow.wav', 'test.txt']
     expected = True
-    result = u.is_audio_homogeneous(test_list)
+    result = u._is_audio_homogeneous(test_list)
     assert result == expected
 
 def test_is_audio_homogeneous_true2():
@@ -120,5 +127,94 @@ def test_is_audio_homogeneous_true2():
     u = Utilities()
     test_list = ['hello.ogg_opus', 'hello.txt']
     expected = True
-    result = u.is_audio_homogeneous(test_list)
+    result = u._is_audio_homogeneous(test_list)
     assert result == expected
+
+def test_get_root_filename_from_uri():
+    from utilities.utilities import Utilities
+    u = Utilities()
+    expected = 'file'
+    data = 'gs://foo/bar/baz/file.wav'
+    result = u._get_root_filename(data)
+    assert result == expected
+
+def test_get_root_filename_from_file_name():
+    from utilities.utilities import Utilities
+    u = Utilities()
+    expected = 'file'
+    data = 'file.doc'
+    result = u._get_root_filename(data)
+    assert result == expected
+
+def test_get_audio_set():
+    from utilities.utilities import Utilities
+    u = Utilities()
+    expected = {'a', 'b', 'c'}
+    data = ['a.wav', 'foo.txt', 'c.txt', 'b.wav', 'c.wav', 'a.txt', 'b.txt']
+    result = u._get_audio_set(data)
+    assert result == expected
+
+def test_get_ref_set():
+    from utilities.utilities import Utilities
+    u = Utilities()
+    expected = {'1', '2', '3'}
+    data = ['hello.wav', 'gs://blah/1.txt', 'foo.doc', 'bar.flac', '3.txt', '2.txt']
+    result = u._get_ref_set(data)
+    assert result == expected
+
+def test_is_paired_false():
+    from utilities.utilities import Utilities
+    u = Utilities()
+    expected = False
+    txt_file = 'gs://baz/fred.txt'
+    all_files = ['bar.wav', 'gs://baz/fred.txt', 'baz.txt', 'zed.ogg', 'foo.raw']
+    result = u._is_paired(txt_file, all_files)
+    assert result == expected
+
+def test_is_paired_true():
+    from utilities.utilities import Utilities
+    u = Utilities()
+    expected = True
+    txt_file = 'fred.txt'
+    all_files = ['bar.wav', 'fred.txt', 'baz.txt', 'fred.flac', 'zed.ogg', 'foo.raw']
+    result = u._is_paired(txt_file, all_files)
+    assert result == expected
+
+def test_validate_files_false1():
+    from utilities.utilities import Utilities
+    u = Utilities()
+    expected_bool = False
+    file_list =  ['bar.wav', 'fred.txt', 'baz.txt', 'gs://fooo/baaar/fred.flac', 'zed.ogg', 'foo.raw']
+    result_bool, result_string = u.are_valid_files(file_list)
+    assert result_bool == expected_bool
+    assert isinstance(result_string, str)
+
+def test_validate_files_false2_invalid_extension():
+    from utilities.utilities import Utilities
+    u = Utilities()
+    expected_bool = False
+    expected_string = 'extension'
+    file_list =  ['bar.wav', 'fred.txt', 'baz.txt', 'fred.flac', 'zed.ogg', 'foo.raw']
+    result_bool, result_string = u.are_valid_files(file_list)
+    assert result_bool == expected_bool
+    assert expected_string in result_string
+
+def test_validate_files_false3_hetero():
+    from utilities.utilities import Utilities
+    u = Utilities()
+    expected_bool = False
+    expected_string = 'heterogeneous'
+    file_list =  ['bar.wav', 'fred.txt', 'baz.txt', 'fred.flac', 'gs://sponge_bob/zed.ogg',]
+    result_bool, result_string = u.are_valid_files(file_list)
+    assert result_bool == expected_bool
+    assert expected_string in result_string
+
+def test_validate_files_false4_no_pair():
+    from utilities.utilities import Utilities
+    u = Utilities()
+    expected_bool = False
+    expected_string = 'paired'
+    file_list =  ['gs://flintstone/fred.txt', 'fred.wav', 'wilma.wav', 'betty.wav', 'betty.txt']
+    result_bool, result_string = u.are_valid_files(file_list)
+    assert result_bool == expected_bool
+    assert expected_string in result_string
--- utilities/utilities.py
+++ utilities/utilities.py
@@ -1,4 +1,6 @@
 class Utilities():
+    supported_audio_extensions = ["flac", "wav", "mp3", "ogg", "oga", "mogg", "opus", "mulaw"]
+
     def string_to_enum(self, encoding):
         from google.cloud.speech_v1p1beta1 import enums
         result = enums.RecognitionConfig.AudioEncoding.ENCODING_UNSPECIFIED
@@ -20,3 +22,93 @@ class Utilities():
             result = enums.RecognitionConfig.AudioEncoding.SPEEX_WITH_HEADER_BYTE
 
         return result
+
+    def _is_valid_file_extension(self, extension):
+        candidate = extension.replace('.', '')
+
+        if candidate.lower() == 'txt':
+            return True
+
+        for ext in self.supported_audio_extensions:
+            if candidate.lower() == ext:
+                return True
+
+        return False
+
+    def _is_audio_homogeneous(self, file_list):
+        extension_set = set()
+        for item in file_list:
+            # ignore text files
+            if 'txt' in item:
+                continue
+            #get just the file ext
+            loc = item.rfind('.')
+            candidate = item[loc+1:]
+            #create a set of unique file ext
+            extension_set.add(candidate)
+        # if more than one audio file ext, have a hetero bucket
+        if len(extension_set) > 1:
+            return False
+        return True
+
+    def _get_root_filename(self, uri):
+        last_slash = uri.rfind('/')
+        if last_slash:
+            file_name = uri[last_slash+1:]
+        dot_loc = file_name.rfind('.')
+        if dot_loc:
+            result = file_name[:dot_loc]
+        else:
+            result = file_name
+        return result
+
+    def _get_audio_set(self, file_list):
+        result = set()
+        for file_name in file_list:
+            for ext in self.supported_audio_extensions:
+                if ext in file_name:
+                    result.add(self._get_root_filename(file_name))
+        return result
+
+    def _get_ref_set(self, file_list):
+        result = set()
+        for file_name in file_list:
+            if '.txt' in file_name:
+                result.add(self._get_root_filename(file_name))
+        return result
+
+    def _is_paired(self, text_file_name, file_list):
+        # get audio list
+        audio_files = self._get_audio_set(file_list)
+        txt_root = self._get_root_filename(text_file_name)
+        for audio in audio_files:
+            if txt_root in audio:
+                return True
+        return False
+
+    def _get_extension(self, file_name):
+        dot_loc = file_name.rfind('.')
+        return file_name[dot_loc+1:]
+
+    def are_valid_files(self, file_list):
+        # check for valid file extensions
+        for file in file_list:
+            ext = self._get_extension(file)
+            if not self._is_valid_file_extension(ext):
+                return False, f'{ext} is not a supported file extension'
+
+        # check all audio same file type
+        if not self._is_audio_homogeneous(file_list):
+            return False, f'Storage endpoint contains heterogeneous audio, must be homogeneous.  Must contain only one audio file type'
+
+        # check that each audio file has a reference text pair
+        audio_set = self._get_audio_set(file_list)
+        ref_set = self._get_ref_set(file_list)
+        mismatch = audio_set.intersection(ref_set)
+        if mismatch:
+            return False, f'Audio files exist with no paired reference human transcription text files'
+
+        return True, None
+
+
+
