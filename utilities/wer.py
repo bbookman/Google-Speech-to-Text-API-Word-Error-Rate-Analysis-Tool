6d306429dd74d706ced26804552487df10a87d6d
Bruce Bookman <bruce.bookman@gmail.com>
2020-02-05 12:32:21 -0800

    replaced wer with v2

--- utilities/wer.py
+++ utilities/wer.py
@@ -13,69 +13,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Evalute the word error rate (WER) for ASR tasks.
+"""The new version script to evalute the word error rate (WER) for ASR tasks.
 
+Tensorflow and Lingvo are not required to run this script.
 
-  - ``ComputeWER(hyp, ref)``    compute WER for one pair of hypothesis/reference
-  - ``AverageWERs(hyps, refs)`` average WER for a list of hypotheses/references
+Example of Usage:
 
-Note to evaluate the ASR, we consider the following pre-processing:
+a) `python simple_wer_v2.py file_hypothesis file_reference`
+b) `python simple_wer_v2.py file_hypothesis file_reference file_keyphrases`
 
-  - change transcripts to lower-case
-  - remove punctuation: ``" , . ! ? (  ) [ ]``
-  - remove extra empty spaces
+where `file_hypothesis` is the filename for hypothesis text,
+`file_reference` is the filename for reference text, and
+`file_keyphrases` is the optional filename for important phrases
+(one phrase per line).
+
+Note that the program will also generate a html to diagnose the errors,
+and the html filename is `{$file_hypothesis}_diagnois.html`.
+
+Another way is to use this file as a stand-alone library, by calling class
+SimpleWER with the following member functions:
+
+- AddHypRef(hyp, ref): Updates the evaluation for each (hyp,ref) pair.
+- GetWER(): Computes word error rate (WER) for all the added hyp-ref pairs.
+- GetSummaries(): Generates strings to summarize word and key phrase errors.
+- GetKeyPhraseStats(): Measures stats for key phrases.
+    Stats include:
+    (1) Jaccard similarity: https://en.wikipedia.org/wiki/Jaccard_index.
+    (2) F1 score: https://en.wikipedia.org/wiki/Precision_and_recall.
+
 """
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import re
 import sys
-
 from six.moves import range
-from six.moves import zip
 
 
-def ComputeEditDistanceMatrix(hs, rs):
-  """Compute edit distance between two list of strings.
-
-  Args:
-    hs: the list of words in the hypothesis sentence
-    rs: the list of words in the reference sentence
-
-  Returns:
-    Edit distance matrix (in the format of list of lists), where the first
-    index is the reference and the second index is the hypothesis.
-  """
-  dr, dh = len(rs) + 1, len(hs) + 1
-  dists = [[]] * dr
-
-  # Initialization.
-  for i in range(dr):
-    dists[i] = [0] * dh
-    for j in range(dh):
-      if i == 0:
-        dists[0][j] = j
-      elif j == 0:
-        dists[i][0] = i
-
-  # Do dynamic programming.
-  for i in range(1, dr):
-    for j in range(1, dh):
-      if rs[i - 1] == hs[j - 1]:
-        dists[i][j] = dists[i - 1][j - 1]
-      else:
-        tmp0 = dists[i - 1][j - 1] + 1
-        tmp1 = dists[i][j - 1] + 1
-        tmp2 = dists[i - 1][j] + 1
-        dists[i][j] = min(tmp0, tmp1, tmp2)
-
-  return dists
-
-
-def PreprocessTxtBeforeWER(txt):
-
+def TxtPreprocess(txt):
   """Preprocess text before WER caculation."""
 
   # Lowercase, remove \t and new line.
@@ -99,7 +75,16 @@ def PreprocessTxtBeforeWER(txt):
   return txt
 
 
-def _GenerateAlignedHtml(hyp, ref, err_type):
+def RemoveCommentTxtPreprocess(txt):
+  """Preprocess text and remove comments in the brancket, such as [comments]."""
+
+  # Remove comments surrounded by box brackets:
+  txt = re.sub(r'\[\w+\]', '', txt)
+
+  return TxtPreprocess(txt)
+
+
+def HighlightAlignedHtml(hyp, ref, err_type):
   """Generate a html element to highlight the difference between hyp and ref.
 
   Args:
@@ -109,13 +94,20 @@ def _GenerateAlignedHtml(hyp, ref, err_type):
 
   Returns:
     a html string where disagreements are highlighted.
-      - hyp highlighted in green, and marked with <del> </del>
-      - ref highlighted in yellow
+      Note `hyp` is highlighted in green, and marked with <del> </del>
+      `ref` is highlighted in yellow. If you want html with nother styles,
+      consider to write your own function.
 
+  Raises:
+    ValueError: if err_type is not among ['none', 'sub', 'del', 'ins'].
+      or if when err_type == 'none', hyp != ref
   """
 
   highlighted_html = ''
   if err_type == 'none':
+    if hyp != ref:
+      raise ValueError('hyp (%s) does not match ref (%s) for none error' %
+                       (hyp, ref))
     highlighted_html += '%s ' % hyp
 
   elif err_type == 'sub':
@@ -139,151 +131,283 @@ def _GenerateAlignedHtml(hyp, ref, err_type):
   return highlighted_html
 
 
-def GenerateSummaryFromErrs(nref, errs):
-  """Generate strings to summarize word errors.
+def ComputeEditDistanceMatrix(hyp_words, ref_words):
+  """Compute edit distance between two list of strings.
 
   Args:
-    nref: integer of total words in references
-    errs: dict of three types of errors. e.g. {'sub':10, 'ins': 15, 'del': 3}
+    hyp_words: the list of words in the hypothesis sentence
+    ref_words: the list of words in the reference sentence
 
   Returns:
-    Two strings:
-    - string summarizing total error, total word, WER,
-    - string breaking down three errors: deleting, insertion, substitute
+    Edit distance matrix (in the format of list of lists), where the first
+    index is the reference and the second index is the hypothesis.
+  """
+  reference_length_plus = len(ref_words) + 1
+  hypothesis_length_plus = len(hyp_words) + 1
+  edit_dist_mat = [[]] * reference_length_plus
+
+  # Initialization.
+  for i in range(reference_length_plus):
+    edit_dist_mat[i] = [0] * hypothesis_length_plus
+    for j in range(hypothesis_length_plus):
+      if i == 0:
+        edit_dist_mat[0][j] = j
+      elif j == 0:
+        edit_dist_mat[i][0] = i
+
+  # Do dynamic programming.
+  for i in range(1, reference_length_plus):
+    for j in range(1, hypothesis_length_plus):
+      if ref_words[i - 1] == hyp_words[j - 1]:
+        edit_dist_mat[i][j] = edit_dist_mat[i - 1][j - 1]
+      else:
+        tmp0 = edit_dist_mat[i - 1][j - 1] + 1
+        tmp1 = edit_dist_mat[i][j - 1] + 1
+        tmp2 = edit_dist_mat[i - 1][j] + 1
+        edit_dist_mat[i][j] = min(tmp0, tmp1, tmp2)
+
+  return edit_dist_mat
+
+
+class SimpleWER(object):
+  """Compute word error rates after the alignment.
+
+  Attributes:
+    key_phrases: list of important phrases.
+    aligned_htmls: list of diagnois htmls, each of which corresponding to a pair
+      of hypothesis and reference.
+    hyp_keyphrase_counts: dict. `hyp_keyphrase_counts[w]` counts how often a key
+      phrases `w` appear in the hypotheses.
+    ref_keyphrase_counts: dict. `ref_keyphrase_counts[w]` counts how often a key
+      phrases `w` appear in the references.
+    matched_keyphrase_counts: dict. `matched_keyphrase_counts[w]` counts how
+      often a key phrase `w` appear in the aligned transcripts when the
+      reference and hyp_keyphrase match.
+    wer_info: dict with four keys: 'sub' (substitution error), 'ins' (insersion
+      error), 'del' (deletion error), 'nw' (number of words). We can use
+      wer_info to compute word error rate (WER) as
+      (wer_info['sub']+wer_info['ins']+wer_info['del'])*100.0/wer_info['nw']
   """
 
-  total_error = sum(errs.values())
-  str_sum = 'total error = %d, total word = %d, wer = %.2f%%' % (
-      total_error, nref, total_error * 100.0 / nref)
-  wer = total_error * 100.0 / nref
-  str_details = 'Error breakdown: del = %.2f%%, ins=%.2f%%, sub=%.2f%%' % (
-      errs['del'] * 100.0 / nref, errs['ins'] * 100.0 / nref,
-      errs['sub'] * 100.0 / nref)
+  def __init__(self,
+               key_phrases=None,
+               html_handler=HighlightAlignedHtml,
+               preprocess_handler=RemoveCommentTxtPreprocess):
+    """Initialize SimpleWER object.
 
-  return str_sum, str_details, wer
+    Args:
+      key_phrases:  list of strings as important phrases. If key_phrases is
+        None, no key_phrases related metric will be computed.
+      html_handler: function to generate a string with html tags.
+      preprocess_handler: function to preprocess text before computing WER.
+    """
+    self._preprocess_handler = preprocess_handler
+    self._html_handler = html_handler
+    self.key_phrases = key_phrases
+    self.aligned_htmls = []
+    self.wer_info = {'sub': 0, 'ins': 0, 'del': 0, 'nw': 0}
+    if key_phrases:
+      # Pre-process key_phrase list
+      if self._preprocess_handler:
+        self.key_phrases = \
+            [self._preprocess_handler(k) for k in self.key_phrases]
 
-
-def ComputeWER(hyp, ref, diagnosis=False):
-  """Computes WER for ASR by ignoring diff of punctuation, space, captions.
-
-  Args:
-    hyp: Hypothesis string.
-    ref: Reference string.
-    diagnosis (optional): whether to generate diagnosis str (in html format)
-
-  Returns:
-    A tuple of 3 elements:
-
-    - dict of three types of errors. e.g. ``{'sub':0, 'ins': 0, 'del': 0}``
-    - num of reference words, integer
-    - aligned html string for diagnois (empty if diagnosis = False)
-  """
-  total_errors = 0
-  hyp = PreprocessTxtBeforeWER(hyp)
-  ref = PreprocessTxtBeforeWER(ref)
-
-
-  # Compute edit distance.
-  hs = hyp.split()
-  rs = ref.split()
-  distmat = ComputeEditDistanceMatrix(hs, rs)
-
-  # Back trace, to distinguish different errors: insert, deletion, substitution.
-  ih, ir = len(hs), len(rs)
-  errs = {'sub': 0, 'ins': 0, 'del': 0}
-  aligned_html = ''
-  while ih > 0 or ir > 0:
-    err_type = ''
-
-    # Distinguish error type by back tracking
-    if ir == 0:
-      err_type = 'ins'
-    elif ih == 0:
-      err_type = 'del'
+      # Init keyphrase_counts for every key phrase
+      self.ref_keyphrase_counts = {}
+      self.hyp_keyphrase_counts = {}
+      self.matched_keyphrase_counts = {}
+      for k in self.key_phrases:
+        self.ref_keyphrase_counts[k] = 0
+        self.hyp_keyphrase_counts[k] = 0
+        self.matched_keyphrase_counts[k] = 0
     else:
-      if hs[ih - 1] == rs[ir - 1]:  # correct
-        err_type = 'none'
-      elif distmat[ir][ih] == distmat[ir - 1][ih - 1] + 1:  # substitute
-        err_type = 'sub'
-      elif distmat[ir][ih] == distmat[ir - 1][ih] + 1:  # deletion
-        err_type = 'del'
-      elif distmat[ir][ih] == distmat[ir][ih - 1] + 1:  # insert
+      self.ref_keyphrase_counts = None
+      self.hyp_keyphrase_counts = None
+      self.matched_keyphrase_counts = None
+
+  def AddHypRef(self, hypothesis, reference):
+    """Update WER when adding one pair of strings: (hypothesis, reference).
+
+    Args:
+      hypothesis: Hypothesis string.
+      reference: Reference string.
+
+    Raises:
+      ValueError: when the program fails to parse edit distance matrix.
+    """
+    if self._preprocess_handler:
+      hypothesis = self._preprocess_handler(hypothesis)
+      reference = self._preprocess_handler(reference)
+
+    # Compute edit distance.
+    hyp_words = hypothesis.split()
+    ref_words = reference.split()
+    distmat = ComputeEditDistanceMatrix(hyp_words, ref_words)
+
+    # Back trace, to distinguish different erroref_words: ins, del, sub.
+    pos_hyp, pos_ref = len(hyp_words), len(ref_words)
+    wer_info = {'sub': 0, 'ins': 0, 'del': 0, 'nw': len(ref_words)}
+    aligned_html = ''
+    matched_ref = ''
+    while pos_hyp > 0 or pos_ref > 0:
+      err_type = ''
+
+      # Distinguish error type by back tracking
+      if pos_ref == 0:
         err_type = 'ins'
+      elif pos_hyp == 0:
+        err_type = 'del'
       else:
-        raise ValueError('fail to parse edit distance matrix')
+        if hyp_words[pos_hyp - 1] == ref_words[pos_ref - 1]:
+          err_type = 'none'  # correct error
+        elif distmat[pos_ref][pos_hyp] == distmat[pos_ref - 1][pos_hyp - 1] + 1:
+          err_type = 'sub'  # substitute error
+        elif distmat[pos_ref][pos_hyp] == distmat[pos_ref - 1][pos_hyp] + 1:
+          err_type = 'del'  # deletion error
+        elif distmat[pos_ref][pos_hyp] == distmat[pos_ref][pos_hyp - 1] + 1:
+          err_type = 'ins'  # insersion error
+        else:
+          raise ValueError('fail to parse edit distance matrix.')
 
-    # Generate aligned_html
-    if diagnosis:
-      if ih == 0 or not hs:
-        tmph = ' '
-      else:
-        tmph = hs[ih - 1]
-      if ir == 0 or not rs:
-        tmpr = ' '
-      else:
-        tmpr = rs[ir - 1]
-      aligned_html = _GenerateAlignedHtml(tmph, tmpr, err_type) + aligned_html
+      # Generate aligned_html
+      if self._html_handler:
+        if pos_hyp == 0 or not hyp_words:
+          tmph = ' '
+        else:
+          tmph = hyp_words[pos_hyp - 1]
+        if pos_ref == 0 or not ref_words:
+          tmpr = ' '
+        else:
+          tmpr = ref_words[pos_ref - 1]
+        aligned_html = self._html_handler(tmph, tmpr, err_type) + aligned_html
 
-    # If no error, go to previous ref and hyp.
-    if err_type == 'none':
-      ih, ir = ih - 1, ir - 1
-      continue
+      # If no error, go to previous ref and hyp.
+      if err_type == 'none':
+        matched_ref = hyp_words[pos_hyp - 1] + ' ' + matched_ref
+        pos_hyp, pos_ref = pos_hyp - 1, pos_ref - 1
+        continue
 
-    # Update error.
-    errs[err_type] += 1
+      # Update error.
+      wer_info[err_type] += 1
 
-    # Adjust position of ref and hyp.
-    if err_type == 'del':
-      ir = ir - 1
-    elif err_type == 'ins':
-      ih = ih - 1
-    else:  # err_type == 'sub'
-      ih, ir = ih - 1, ir - 1
+      # Adjust position of ref and hyp.
+      if err_type == 'del':
+        pos_ref = pos_ref - 1
+      elif err_type == 'ins':
+        pos_hyp = pos_hyp - 1
+      else:  # err_type == 'sub'
+        pos_hyp, pos_ref = pos_hyp - 1, pos_ref - 1
 
-  assert distmat[-1][-1] == sum(errs.values())
+    # Verify the computation of edit distance finishes
+    assert distmat[-1][-1] == wer_info['ins'] + \
+        wer_info['del'] + wer_info['sub']
 
-  # Num of words. For empty ref we set num = 1.
-  nref = max(len(rs), 1)
-  total_errors = sum(errs.values())
-  return errs, nref, aligned_html, total_errors
+    # Accumulate err_info before the next (hyp, ref).
+    for k in wer_info:
+      self.wer_info[k] += wer_info[k]
+
+    # Collect aligned_htmls.
+    if self._html_handler:
+      self.aligned_htmls += [aligned_html]
+
+    # Update key phrase info.
+    if self.key_phrases:
+      for w in self.key_phrases:
+        self.ref_keyphrase_counts[w] += reference.count(w)
+        self.hyp_keyphrase_counts[w] += hypothesis.count(w)
+        self.matched_keyphrase_counts[w] += matched_ref.count(w)
+
+  def GetWER(self):
+    """Compute Word Error Rate (WER) to summarize word erroref_words.
+
+    Note WER can be larger than 100.0, esp when there are many insertion errors.
+
+    Returns:
+      WER as percentage number, usually between 0.0 to 100.0
+    """
+    nref = self.wer_info['nw']
+    nref = max(1, nref)  # non_zero value for division
+    total_error = self.wer_info['ins'] \
+        + self.wer_info['del'] + self.wer_info['sub']
+    return total_error * 100.0 / nref
+
+  def GetKeyPhraseStats(self):
+    """Measure the Jaccard similarity of key phrases between hyps and refs.
+
+    Returns:
+      jaccard_similarity: jaccard similarity, between 0.0 and 1.0
+      F1_keyphrase:  F1 score (=2/(1/prec + 1/recall)), between 0.0 and 1.0
+      matched_keyphrases: num of matched key phrases.
+      ref_keyphrases:  num of key phrases in the reference strings.
+      hyp_keyphrases:  num of key phrases in the hypothesis strings.
+    """
+
+    matched_k = sum(self.matched_keyphrase_counts.values())
+    ref_k = sum(self.ref_keyphrase_counts.values())
+    hyp_k = sum(self.hyp_keyphrase_counts.values())
+    joined_k = ref_k + hyp_k - matched_k
+    joined_k = max(1, joined_k)  # non_zero value for division
+    jaccard_similarity = matched_k * 1.0 / joined_k
+
+    f1_k = 2.0 * matched_k / max(ref_k + hyp_k, 1.0)
+    return (jaccard_similarity, f1_k, matched_k, ref_k, hyp_k)
+
+  def GetSummaries(self):
+    """Generate strings to summarize word errors and key phrase errors.
+
+    Returns:
+      str_sum: string summarizing total error, total word and WER.
+      str_details: string breaking down three error types: del, ins, sub.
+      str_str_keyphrases_info: string summarizing kerphrase information.
+    """
+    nref = self.wer_info['nw']
+    total_error = self.wer_info['ins'] \
+        + self.wer_info['del'] + self.wer_info['sub']
+    str_sum = 'total WER = %d, total word = %d, wer = %.2f%%' % (
+        total_error, nref, self.GetWER())
+
+    str_details = 'Error breakdown: del = %.2f%%, ins=%.2f%%, sub=%.2f%%' % (
+        self.wer_info['del'] * 100.0 / nref, self.wer_info['ins'] * 100.0 /
+        nref, self.wer_info['sub'] * 100.0 / nref)
+
+    str_keyphrases_info = ''
+    if self.key_phrases:
+      jaccard_p, f1_p, matched_p, ref_p, hyp_p = self.GetKeyPhraseStats()
+      str_keyphrases_info = ('matched %d key phrases (%d in ref, %d in hyp), '
+                             'jaccard similarity=%.2f, F1=%.2f') % \
+                              (matched_p, ref_p, hyp_p, jaccard_p, f1_p)
+
+    return str_sum, str_details, str_keyphrases_info
 
 
-def AverageWERs(hyps, refs, verbose=True, diagnosis=False):
-  """Computes average WER from a list of references/hypotheses.
+def main(argv):
+  hypothesis = open(argv[1], 'r').read()
+  reference = open(argv[2], 'r').read()
 
-  Args:
-    hyps: list of hypothesis strings.
-    refs: list of reference strings.
-    verbose: optional (default True)
-    diagnosis (optional): whether to generate list of diagnosis html
+  if len(argv) == 4:
+    phrase_lines = open(argv[3]).readlines()
+    keyphrases = [line.strip() for line in phrase_lines]
+  else:
+    keyphrases = None
 
-  Returns:
-    A tuple of 3 elements:
+  wer_obj = SimpleWER(
+      key_phrases=keyphrases,
+      html_handler=HighlightAlignedHtml,
+      preprocess_handler=RemoveCommentTxtPreprocess)
 
-    - dict of three types of errors. e.g. ``{'sub':0, 'ins': 0, 'del': 0}``
-    - num of reference words, integer
-    - list of aligned html string for diagnosis (empty if diagnosis = False)
+  wer_obj.AddHypRef(hypothesis, reference)
 
-  """
-  totalw = 0
-  total_errs = {'sub': 0, 'ins': 0, 'del': 0}
-  aligned_html_list = []
+  str_summary, str_details, str_keyphrases_info = wer_obj.GetSummaries()
+  print(str_summary)
+  print(str_details)
+  print(str_keyphrases_info)
 
-  for hyp, ref in zip(hyps, refs):
-    errs_i, nref_i, diag_str, x = ComputeWER(hyp, ref, diagnosis)
-    if diagnosis:
-      aligned_html_list += [diag_str]
-
-    totalw += nref_i
-    total_errs['sub'] += errs_i['sub']
-    total_errs['ins'] += errs_i['ins']
-    total_errs['del'] += errs_i['del']
-
-  if verbose:
-    str_summary, str_details = GenerateSummaryFromErrs(totalw, total_errs)
-    print(str_summary)
-    print(str_details)
-
-  return total_errs, totalw, aligned_html_list
-
-
+  try:
+    fn_output = argv[1] + '_diagnosis.html'
+    aligned_html = '<br>'.join(wer_obj.aligned_htmls)
+    with open(fn_output, 'wt') as fp:
+      fp.write('<body><html>')
+      fp.write('<div>%s</div>' % aligned_html)
+      fp.write('</body></html>')
+  except IOError:
+    print('failed to write diagnosis html')
