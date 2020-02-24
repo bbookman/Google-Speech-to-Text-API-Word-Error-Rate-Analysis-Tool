8335a7cfed49b083b2bd3423567794057dc0849c
Bruce Bookman <bruce.bookman@gmail.com>
2020-02-24 12:39:36 -0800

    changed alts from t f to actual alts

--- utilities/io_handler.py
+++ utilities/io_handler.py
@@ -47,7 +47,7 @@ class IOHandler(object):
 
         full_path = f'{self.get_result_path()}/{self._result_file_name}'
         string = f'{uri}, {configuration.get_model()}, {configuration.get_use_enhanced()}, {configuration.get_language_code()},' \
-                 f' {bool(configuration.get_alternative_language_codes())}, {bool(configuration.get_speech_context()) and configuration.get_boost()},' \
+                 f' {(configuration.get_alternative_language_codes())}, {bool(configuration.get_speech_context())},' \
                  f'{configuration.get_boost()}, {ref_total_word_count}, {ref_error_count}, {word_error_rate}, {nlp_model.get_apply_stemming()},' \
                  f'{nlp_model.get_remove_stop_words()}, {nlp_model.get_n2w()}, {nlp_model.get_expand_contractions()}, {ins}, {deletions}, {subs}' \
                  f'{deleted_words}, {inserted_words}, {substitute_words}\n'
